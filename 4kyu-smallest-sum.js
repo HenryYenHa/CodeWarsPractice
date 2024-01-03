590ad63cda223abf164bf7bbe11cc63c2864478e
HenryYenHa <henryha@live.ca>
2024-01-02 19:20:03 -0700

    Solution fails; it times out

--- 4kyu-smallest-sum.js
+++ 4kyu-smallest-sum.js
@@ -1,17 +1,23 @@
 function solution(numbers) {
-  let factorList = [];
-  let fListIndex = 0;
-  for (let i = 1; i < numbers[0]; i++) {
+  console.log("START:", numbers);
+  let factorList = [1];
+  let fListIndex = 1;
+  for (let i = 2; i < numbers[0] + 1; i++) {
     if (numbers[0] % i == 0) {
       factorList[fListIndex] = i;
       fListIndex++;
     }
   }
+
   for (num of numbers) {
     for (factor of factorList) {
       if (factorList.length == 1) {
         console.log("primedOutnow");
         return numbers.length;
+      }
+      if (factor > num) {
+        console.log("removed: ", +factor, factorList);
+        factorList.splice(factorList.indexOf(factor), 1);
       }
       if (num % factor != 0) {
         console.log("removed: ", +factor, factorList);
@@ -19,6 +25,7 @@ function solution(numbers) {
       }
     }
   }
+  console.log("final: ", factorList, numbers.length);
   return factorList.pop() * numbers.length;
 }
 
