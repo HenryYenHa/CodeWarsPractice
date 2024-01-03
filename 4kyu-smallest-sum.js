4f814139ed35198f70af060ead6d491deb55b80e
HenryYenHa <henryha@live.ca>
2024-01-02 18:58:36 -0700

    4-kyu smallest check

---
+++ 4kyu-smallest-sum.js
@@ -0,0 +1,25 @@
+function solution(numbers) {
+  let factorList = [];
+  let fListIndex = 0;
+  for (let i = 1; i < numbers[0]; i++) {
+    if (numbers[0] % i == 0) {
+      factorList[fListIndex] = i;
+      fListIndex++;
+    }
+  }
+  for (num of numbers) {
+    for (factor of factorList) {
+      if (factorList.length == 1) {
+        console.log("primedOut");
+        return numbers.length;
+      }
+      if (num % factor != 0) {
+        console.log("removed: ", +factor, factorList);
+        factorList.splice(factorList.indexOf(factor), 1);
+      }
+    }
+  }
+  return factorList.pop() * numbers.length;
+}
+
+console.log("THEEND:", solution([999, 6, 9, 21]));
